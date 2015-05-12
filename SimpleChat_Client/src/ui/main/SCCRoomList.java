5aa8eb8fa0c80d983980f3c6c4bc7633538f03be
Leemw <Leemw@Leemw-PC>
2015-05-12 19:43:27 +0900

    SCCRoomList class upload- setMainPanel의 기능 : SCCRoomList와 SCCMainPanel의 개체 생성시 두개가 서로 연관이 있으므로 공유하도록 해주는 기능 이외 방을 클릭하거나 더블클릭하여 퇴장 여부를 하기 위한 mouseListener 구현

---
+++ SimpleChat_Client/src/ui/main/SCCRoomList.java
@@ -0,0 +1,53 @@
+package ui.main;
+
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JList;
+
+
+public class SCCRoomList extends JList implements MouseListener{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+
+	private SCCMainPanel mainPanel;
+
+	public void setMainPanel(SCCMainPanel mainPanel) {
+		// TODO Auto-generated method stub
+		this.mainPanel = mainPanel;
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
