c49735447b8a7c3de28215ec12c5edf3e4f860c1
Leemw <Leemw@Leemw-PC>
2015-05-12 19:11:20 +0900

    ServerManagerPanel class upload
    추후 계획 / Server start, GUI 누르면서 생기는 명령어 set 작성

---
+++ SimpleChat_Server/src/net/main/ServerManagerPanel.java
@@ -0,0 +1,31 @@
+package net.main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+public class ServerManagerPanel extends JPanel implements ActionListener, Runnable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
