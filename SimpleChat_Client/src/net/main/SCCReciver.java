f4bffe3dfbccddb5fd3379a0a782bb6b35832983
Leemw <Leemw@Leemw-PC>
2015-05-12 18:42:02 +0900

    SimpleChat_Client etc

---
+++ SimpleChat_Client/src/net/main/SCCReciver.java
@@ -0,0 +1,48 @@
+package net.main;
+
+import java.io.ObjectInputStream;
+import java.util.Vector;
+
+import object.shared.SCPacket;
+import object.shared.SCRoom;
+import ui.main.SCCMainPanel;
+
+public class SCCReciver extends Thread implements Runnable{
+
+	private ObjectInputStream objInputStream;
+	private SCCMainPanel mainPanel;
+
+	public SCCReciver(ObjectInputStream objInputStream, SCCMainPanel mainPanel) {
+		super();
+		this.objInputStream = objInputStream;
+		this.mainPanel = mainPanel;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void run(){
+		while(true){
+			SCPacket packet = null;
+			try{
+				packet = (SCPacket)objInputStream.readObject();
+				if(packet.getMessage().equals("connectionSuccess")){
+					mainPanel.setRoom((Vector<SCRoom>)packet.getArgs()[0]);
+					mainPanel.writeMessage(packet.getMessage());
+				}
+				else if(packet.getMessage().equals("setRoom")){
+					mainPanel.setRoom((Vector<SCRoom>)packet.getArgs()[0]);
+				}
+				else if(packet.getMessage().equals("enterRoom")){
+					mainPanel.enterRoom((SCRoom)packet.getArgs()[0]);
+				}
+				else if(packet.getMessage().equals("readMessage")){
+					mainPanel.writeMessage((String)packet.getArgs()[0]);
+				}
+				else if(packet.getMessage().equals("Annoucement")){
+					mainPanel.Announcement_writeMessage((String)packet.getArgs()[0]);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
