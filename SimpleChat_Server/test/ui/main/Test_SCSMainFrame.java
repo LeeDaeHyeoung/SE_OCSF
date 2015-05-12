3742bef8e34de766c28ac164412bded13bc85502
Leemw <Leemw@Leemw-PC>
2015-05-12 21:21:24 +0900

    test_case SCSMainFrame class test upload

---
+++ SimpleChat_Server/test/ui/main/Test_SCSMainFrame.java
@@ -0,0 +1,15 @@
+package ui.main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_SCSMainFrame {
+
+	@Test
+	public void test_() {
+		SCSMainFrame scsmainframe = new SCSMainFrame();
+		scsmainframe.runServer();
+	}
+
+}
