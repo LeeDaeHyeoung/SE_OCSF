c2d553b0c939045cace7c7b05a6c401c5407ca07
Leemw <Leemw@Leemw-PC>
2015-05-18 10:53:22 +0900

    setRoomlist의 model에 방의 정보를 추가적으로 저장하는 기능

--- SimpleChat_Client/src/ui/main/SCCRoomList.java
+++ SimpleChat_Client/src/ui/main/SCCRoomList.java
@@ -3,7 +3,12 @@ package ui.main;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Vector;
+
+import javax.swing.DefaultListModel;
 import javax.swing.JList;
+
+import object.shared.SCRoom;
 
 
 public class SCCRoomList extends JList implements MouseListener{
@@ -11,12 +16,18 @@ public class SCCRoomList extends JList implements MouseListener{
 	
 	private static final long serialVersionUID = 1L;
 	
-
+	private DefaultListModel model;
 	private SCCMainPanel mainPanel;
 
 	public void setMainPanel(SCCMainPanel mainPanel) {
 		// TODO Auto-generated method stub
 		this.mainPanel = mainPanel;
+	}
+	public void setRoomList(Vector<SCRoom> roomList) {
+		model.removeAllElements();
+		for(SCRoom room: roomList){
+			model.addElement(room);
+		}
 	}
 	
 	@Override
